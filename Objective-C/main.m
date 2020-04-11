d25496bbad41f24df10f54c7dbea3a914a66c655
YuryPodkutin <yurapodkutin@yandex.ru>
2020-04-11 15:58:42 +0700

    среднее трех чисел

--- Objective-C/main.m
+++ Objective-C/main.m
@@ -8,10 +8,24 @@
 
 #import <Foundation/Foundation.h>
 
+float averageOfThree(int a, int b, int c) {
+    return (a + b + c) / 3.0;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        int first = 0;
+        int second = 0;
+        int third = 0;
+        
+        printf("Введите первое число: ");
+        scanf("%d", &first);
+        printf("Введите второе число: ");
+        scanf("%d", &second);
+        printf("Введите третье число: ");
+        scanf("%d", &third);
+        float result = averageOfThree(first, second, third);
+        printf("Среднее трех чисел %d, %d, %d = %.2f\n", first, second, third, result);
     }
     return 0;
 }
