b381daddeff66084a415f531dc459e52b3a04d96
YuryPodkutin <yurapodkutin@yandex.ru>
2020-04-11 11:05:38 +0700

    первый коммит

---
+++ Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C
+//
+//  Created by Yury Podkutin on 11.04.2020.
+//  Copyright © 2020 Yury Podkutin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
